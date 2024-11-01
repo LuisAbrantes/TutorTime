21114c6bb75a4896cbab1c4ba719c9534659d69b
vinicius <vinicius.maximo@aluno.ifsp.edu.br>
2024-11-01 16:59:07 -0300

    Updated TutorTime

--- app.js
+++ app.js
@@ -3,6 +3,126 @@ const app = express()
 const bodyParser = require('body-parser')
 const handlebars = require('express-handlebars')
 const Monitorias = require('./models/monitorias')
+const todasDisciplinas = [
+    // Original
+    'Algoritmos e Lógica de Programação',
+    'Programação Orientada a Objetos',
+    'Estruturas de Dados',
+    'Banco de Dados',
+    'Sistemas Operacionais',
+    'Redes de Computadores',
+    'Engenharia de Software',
+    'Desenvolvimento Web',
+    'Inteligência Artificial',
+    'Análise e Projeto de Sistemas',
+    'Teoria da Computação',
+    'Matemática Discreta',
+    'Cálculo',
+    'Física',
+    'Química',
+    'Educação Física',
+    'Inglês',
+    'Empreendedorismo',
+    'Ética e Cidadania',
+    'Arquitetura',
+    'Design de Interiores',
+    
+    // Maiúsculas
+    'ALGORITMOS E LÓGICA DE PROGRAMAÇÃO',
+    'PROGRAMAÇÃO ORIENTADA A OBJETOS',
+    'ESTRUTURAS DE DADOS',
+    'BANCO DE DADOS',
+    'SISTEMAS OPERACIONAIS',
+    'REDES DE COMPUTADORES',
+    'ENGENHARIA DE SOFTWARE',
+    'DESENVOLVIMENTO WEB',
+    'INTELIGÊNCIA ARTIFICIAL',
+    'ANÁLISE E PROJETO DE SISTEMAS',
+    'TEORIA DA COMPUTAÇÃO',
+    'MATEMÁTICA DISCRETA',
+    'CÁLCULO',
+    'FÍSICA',
+    'QUÍMICA',
+    'EDUCAÇÃO FÍSICA',
+    'INGLÊS',
+    'EMPREENDEDORISMO',
+    'ÉTICA E CIDADANIA',
+    'ARQUITETURA',
+    'DESIGN DE INTERIORES',
+    
+    // Minúsculas
+    'algoritmos e lógica de programação',
+    'programação orientada a objetos',
+    'estruturas de dados',
+    'banco de dados',
+    'sistemas operacionais',
+    'redes de computadores',
+    'engenharia de software',
+    'desenvolvimento web',
+    'inteligência artificial',
+    'análise e projeto de sistemas',
+    'teoria da computação',
+    'matemática discreta',
+    'cálculo',
+    'física',
+    'química',
+    'educação física',
+    'inglês',
+    'empreendedorismo',
+    'ética e cidadania',
+    'arquitetura',
+    'design de interiores',
+    
+    // Capitalizadas
+    'Algoritmos E Lógica De Programação',
+    'Programação Orientada A Objetos',
+    'Estruturas De Dados',
+    'Banco De Dados',
+    'Sistemas Operacionais',
+    'Redes De Computadores',
+    'Engenharia De Software',
+    'Desenvolvimento Web',
+    'Inteligência Artificial',
+    'Análise E Projeto De Sistemas',
+    'Teoria Da Computação',
+    'Matemática Discreta',
+    'Cálculo',
+    'Física',
+    'Química',
+    'Educação Física',
+    'Inglês',
+    'Empreendedorismo',
+    'Ética E Cidadania',
+    'Arquitetura',
+    'Design De Interiores',
+    
+    // Primeira palavra
+    'Algoritmos',
+    'Programação',
+    'Estruturas',
+    'Banco',
+    'Sistemas',
+    'Redes',
+    'Engenharia',
+    'Desenvolvimento',
+    'Inteligência',
+    'Análise',
+    'Teoria',
+    'Matemática',
+    'Cálculo',
+    'Física',
+    'Química',
+    'Educação',
+    'Inglês',
+    'Empreendedorismo',
+    'Ética',
+    'Arquitetura',
+    'Design',
+    'Administração'
+];
+
+
+
 let c =1
 
 //-Config⚙️
@@ -39,28 +159,38 @@ let c =1
                 res.render('src/manage/manage',{Monitorias:Monitorias})
             })
         })
-    
+    //          HOME >>> MATERIA
             app.get("/home/:materia",function(req,res){
-                res.send(`Materia Selecionada >>> ${req.params.materia}`)
+                Monitorias.findAll({where:{'nome':req.params.materia}}).then(function(Monitorias){
+                    res.render('oi',{Monitorias:Monitorias})
+                })
+            })
+
+            app.get("/TESTE",function(req,res){
+                res.render("oi")
             })
 
 //Registrando em Databases
     //Monitorias
         app.post("/add",function(req,res){
+            if (todasDisciplinas.includes(req.body.nome)) {
+                Monitorias.create({
+                    nome :req.body.nome,
+                    urlimg:req.body.urlimg || "",
+                    cursossug:req.body.cursossug,
+                    profresp:req.body.profresp,
+                    monitoresp:req.body.monitoresp,
+                    diahrs:req.body.diahr,
+                    sala_link:req.body.salink
+                }).then(function(){
+                    res.redirect('/manage')
+                }).catch(function(erro){
+                    res.send("Deu Erro Boy >>>>> "+erro)
+                })
+            }else{
+                console.log("DISCIPLINA NAO ENCONTRADA!!")
+            }
             
-            Monitorias.create({
-                nome :req.body.nome,
-                urlimg:req.body.urlimg || "",
-                cursossug:req.body.cursossug,
-                profresp:req.body.profresp,
-                monitoresp:req.body.monitoresp,
-                diahrs:req.body.diahr,
-                sala_link:req.body.salink
-            }).then(function(){
-                res.redirect('/manage')
-            }).catch(function(erro){
-                res.send("Deu Erro Boy >>>>> "+erro)
-            })
         })
 //Deletando Dados!
     app.get('/deletar/:id',function(req,res){
